450c5300cf9bdb91d22297124af0f70420d8ba05
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-12-02 16:07:26 -0700

    CL Instructions

---
+++ main.rb
@@ -0,0 +1,8 @@
+#instructions
+puts 'Welcome to Tic-Tac-Toe!'
+puts 'Each turn, enter two numbers with a space, per the grid layout below:'
+puts "\r\n"
+puts '0 0 | 0 1 | 0 2'
+puts '1 0 | 1 1 | 1 2'
+puts '2 0 | 2 1 | 2 2'
+puts "\r\n"
