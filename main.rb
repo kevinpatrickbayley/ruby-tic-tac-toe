9ca2c2bcbe0d48d061cca79ab6072dfbd8d606f4
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-12-02 16:13:50 -0700

    Game class initialize method

--- main.rb
+++ main.rb
@@ -1,6 +1,15 @@
 class Game
   @@turn_count = 1
   @@winner = ''
+
+  def initialize
+    puts 'Player 1, enter your name!'
+    @player_one_name = gets.chomp
+    puts 'Player 2, enter your name!'
+    @player_two_name = gets.chomp
+    @board = Array.new(3){Array.new(3,'*')}
+  end
+
 end
 
 #instructions
