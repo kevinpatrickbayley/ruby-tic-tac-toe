d455d32e70a28aacd5937807200ac17bc30a9c78
Kevin Bayley <kevin.p.bayley@gmail.com>
2025-12-04 15:28:56 -0700

    Fix board coord, two spaces 2 2, bug

--- main.rb
+++ main.rb
@@ -14,7 +14,7 @@ class Game
   def display_board(board)
     puts "\r"
     puts "#{board[0][0]} | #{board[0][1]} | #{board[0][2]}"
-    puts "#{board[1][0]} | #{board[1][1]} | #{board[2][2]}"
+    puts "#{board[1][0]} | #{board[1][1]} | #{board[1][2]}"
     puts "#{board[2][0]} | #{board[2][1]} | #{board[2][2]}"
     puts "\r"
   end
